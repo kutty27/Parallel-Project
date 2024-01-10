a40be1c8eb06eeb2279d3023d1e01e18c2c932c8
kutty27 <154492609+kutty27@users.noreply.github.com>
2024-01-10 11:01:24 +0530

    Create SavingsAccount.java

---
+++ SavingsAccount.java
@@ -0,0 +1,18 @@
+public class SavingsAccount extends Account {
+    private double interestRate;
+
+    public SavingsAccount(String accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void applyInterest() {
+        double interest = getBalance() * interestRate / 100;
+        deposit(interest);
+        System.out.println("Interest applied. New balance: " + getBalance());
+    }
+}
